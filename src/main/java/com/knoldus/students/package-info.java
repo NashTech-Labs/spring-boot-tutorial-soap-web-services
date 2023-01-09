e74db8fce5012b62bf443490bb11199db874e0a7
Shashikant-15 <shashikant.tanti@knoldus.com>
2023-01-09 12:34:06 +0530

    initial commit

---
+++ src/main/java/com/knoldus/students/package-info.java
@@ -0,0 +1,3 @@
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://knoldus.com/students", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.in28minutes.students;
